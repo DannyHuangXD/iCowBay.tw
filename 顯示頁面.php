007967983cf697964daa3d11ed691d91d654ad81
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-05 21:17:17 +0800

    resorted the progress

--- 顯示頁面.php
+++ 顯示頁面.php
@@ -2,6 +2,14 @@
 <html>
 <body>
     <?php
+        echo "<tr>" ;
+        echo "<td width='316' height='21' align='center'>留言編號:$sql_id.</td>" ;
+        echo "<td width='316' height='21' align='center'>留言者姓名:$sql_name.</td></tr>" ;
+        echo "<tr><td width='316' height='21' align='center'>MAIL:$sql_email.</td>" ;
+        echo "<td width='316' height='21' align='center'>留言時間:$sql_posttime.</td></tr>" ;
+        echo "<tr><td height='179' colspan='2' align='left' valign='top'>留言內容:$sql_content.</td></tr>" ;
+}
+        echo "</tr>";
         $mysqli = new mysqli('raspi', 'piusr', 'rpi', 'raspi' ,'article');
         if($mysqli -> connect_error{
         die('Connect Error('. $mysqli->connect_errno .')' . $mysqli -> connect-error);
@@ -17,14 +25,6 @@
         $sql_email = $record[ "email"] ;
         $sql_posttime = $record[ "posttime"] ;
         $sql_content = $record[ "content"] ;
-        echo "<tr>" ;
-        echo "<td width='316' height='21' align='center'>留言編號:$sql_id.</td>" ;
-        echo "<td width='316' height='21' align='center'>留言者姓名:$sql_name.</td></tr>" ;
-        echo "<tr><td width='316' height='21' align='center'>MAIL:$sql_email.</td>" ;
-        echo "<td width='316' height='21' align='center'>留言時間:$sql_posttime.</td></tr>" ;
-        echo "<tr><td height='179' colspan='2' align='left' valign='top'>留言內容:$sql_content.</td></tr>" ;
-}
-        echo "</tr>";
         $mysqli->close();
     ?>
 </body>

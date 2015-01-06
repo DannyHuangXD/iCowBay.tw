c1e5b425cd4ee06c60596986264d605267238f54
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-07 04:18:34 +0800

    fixed some stupid mistakes

--- scripts/new_post.php
+++ scripts/new_post.php
@@ -3,5 +3,5 @@
     $artiID = mysqli->query("SELECT COUNT(*) from artiInfo;");
     $insert_sql = "INSERT INTO artiInfo (artiID, artitext)". "VALUES('$artiID','{content}')";
     mysqli_query($insert_sql);
-    echo <p id="success"> SUCCESS! your post id is $artiID </p>
+    echo "SUCCESS! your post id is $artiID.";
 ?>

d366e1e9bbeb0ebc284146a54f513748731bbdf1
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-06 15:42:33 +0800

    search bar

---
+++ search.php
@@ -0,0 +1,10 @@
+<html>
+    <head><meta charset="utf-8"></head>
+    <body>
+        <div id = "search_bar">
+            <p> search: </p>
+            <?php echo $_REQUEST['search']; ?>
+            </br>
+        </div>
+    </body>
+</html>

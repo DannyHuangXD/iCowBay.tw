80672193cbad03224f7a1971e60f9365ebd86e30
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 02:53:33 +0800

    just keep on fixing

--- scripts/search.php
+++ scripts/search.php
@@ -4,19 +4,45 @@
     </head>
     <body>
         <table width = "900" border = "0.5" cellpadding = "1">
-            <p>a</p>
             <?php
                 require "connect.php";
                 $sql_kw = $_REQUEST['sqlkw']; //keyword search variable
-
                 $sql_search = "SELECT * FROM artiInfo WHERE artitext LIKE '%$sql_kw%'";
                 $result = $mysqli->query($sql_search);
                 $rows = mysqli_num_rows($result);
-                mysqli_free_result($result);
                 $pageSize = 3;
                 $pageCount = celi($rows / pageSize);
             ?>
-<p>b</p>
-        </table>
+           <?php if($rows){?>
+            <tr>
+                <td>artiID</td>
+                <td>Content</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <?php while($row = mysqli_fetch_array($result)){ ?>
+            <tr>
+                <td><?=$rows['artiID']?></td>
+                <td><?=$rows['artitext']?></td>
+            </tr>
+            <?php } ?>
+            <tr>
+                <td colspan="2">
+                <?php
+                    if($rows <= $pageSize) echo "1";
+                    else{
+                        for($i = 1; $i  <= pageCount; $i++)
+                            echo "{$i}&nbsp;&nbsp;";
+                    }
+                ?>
+                </td>
+            </tr>
+            <?php } else{ ?>
+            <tr>
+                <td> Your search result is rip in pepperonis</td>
+            </tr>
+            <?php } ?>        </table>
     </body>
 </html>

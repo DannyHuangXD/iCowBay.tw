dc2364bc1818076f892997495b88ff85ee0f5509
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 02:20:41 +0800

    that should fix the shit

--- scripts/search.php
+++ scripts/search.php
@@ -8,7 +8,7 @@
                 require "connect.php";
                 $sql_kw = $_REQUEST['sqlkw']; //keyword search variable
                 echo $sql_kw;
-                $sql_search = "SELECT * FROM artiInfo WHERE artitext LIKE '%$sql_kw%'";
+                $sql_search = "SELECT * FROM artiInfo WHERE artitext LIKE '%{$sql_kw}%'";
                 $result = $mysqli->query($sql_search);
                 $rows = mysqli_num_rows($result);
                 mysqli_free_result($result);

65895d3db8c6f18d477fa68982f045e093edb287
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 01:37:27 +0800

    added ability to show search query results, working on paging feature

--- scripts/search.php
+++ scripts/search.php
@@ -1,5 +1,51 @@
 <?php
     require "connect.php";
     $sql_kw; //keyword search variable
-    $sql_search = mysqli_query("SELECT artitext FROM artiInfo WHERE artitext LIKE '%$sql_kw%'");
+    $sql_search = "SELECT * FROM artiInfo WHERE artitext LIKE '%$sql_kw%'";
+    $result = $mysqli->query($sql_search);
+    $rows = mysqli_num_rows($result);
+    mysqli_free_result($result);
+    $pageSize = 3;
+    $pageCount = celi($rows / pageSize);
+    echo pageCount;
 ?>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <table width = "900" border = "0.5" cellpadding = "1">
+            <?php if($rows){?>
+            <tr>
+                <td>artiID</td>
+                <td>Content</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+                <?php while($row = mysqli_fetch_array($result)){ ?>
+            <tr>
+                <td><?=$row['artiID']?></td>
+                <td><?=$row['artitext']?></td>
+            </tr>
+            <?php } ?>
+            <tr>
+                <td colspan="2">
+                <?php
+                    if($rows <= $pageSize) echo "1";
+                    else{
+                        for($i = 1; $i  <= pageCount; $i++)
+                            echo "{$i}&nbsp;&nbsp;";
+                    }
+                ?>
+                </td>
+            </tr>
+            <?php } else{ ?>
+            <tr>
+                <td> Your search result is rip in pepperonis</td>
+            </tr>
+            <?php } ?>
+        </table>
+    </body>
+</html>

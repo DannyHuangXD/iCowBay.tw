51753f7006b7b27e8322af69301af33cfe43e281
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 11:27:09 +0800

    GGININ

--- scripts/new_post.php
+++ scripts/new_post.php
@@ -1,8 +1,8 @@
 <?php
     require 'connect.php';
-    $artiID = mysqli_query("SELECT COUNT(*) from artiInfo;");
+    $artiID = $mysqli->query("SELECT COUNT(*) from artiInfo;");
     $ID = $_POST[artiID];
     $contents = $_POST[content];
-    $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES('$ID','$contents');";
-    $result = mysqli_query($insert_sql);
+    $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES($ID,$contents);";
+    $result = $mysqli->query($insert_sql);
 ?>

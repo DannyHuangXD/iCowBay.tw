cc73762bdbf182fd9a94582fc69787d2b1c3b3f0
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 12:03:43 +0800

    FIXED

--- scripts/new_post.php
+++ scripts/new_post.php
@@ -2,11 +2,10 @@
 <body>
     <?php
     require 'connect.php';
-    $id = $mysqli->query("SELECT COUNT(*) from artiInfo;");
-    $contents = $_POST["content"];
-echo $id;
-echo $contents;
-    $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES($id,$contents);";
+        $result = $mysqli->query("SELECT * from artiInfo;");
+    $id = 1 + mysqli_num_rows($result);
+    $contents = $_POST['content'];
+    $insert_sql = "INSERT INTO artiInfo" ."(artiID, artitext)"."VALUES('$id', '$contents');";
     $result = $mysqli->query($insert_sql);
 ?>
 </body>

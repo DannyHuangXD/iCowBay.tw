398558e0324daf577edeed6505d340507d9d9571
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 11:42:57 +0800

    testify

--- scripts/new_post.php
+++ scripts/new_post.php
@@ -1,6 +1,13 @@
-<?php
+<html>
+<body>
+    <?php
     require 'connect.php';
+    $id = $mysqli->query("SELECT COUNT(*) from artiInfo;");
     $contents = $_POST["content"];
-    $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES(100,$contents);";
+echo $id;
+echo $contents;
+    $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES($id,$contents);";
     $result = $mysqli->query($insert_sql);
 ?>
+</body>
+</html>

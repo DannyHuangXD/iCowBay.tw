f4872e7459bb43f09e5749215575f0150d181e5d
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 11:00:31 +0800

    fix sending problems

--- scripts/new_post.php
+++ scripts/new_post.php
@@ -2,6 +2,6 @@
     require 'connect.php';
     $artiID = ＄mysqli->query("SELECT COUNT(*) from artiInfo;");
     $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES('$_POST[artiID]','$_POST[content]')";
-    mysqli_query($insert_sql);
+    $mysqli->query($insert_sql);
     echo "SUCCESS! your post id is $artiID.";
 ?>

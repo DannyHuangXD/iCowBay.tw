277dfd3495bef0122642d0764dd7163489ed16f7
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 11:30:28 +0800

    fuck you

--- scripts/new_post.php
+++ scripts/new_post.php
@@ -1,8 +1,8 @@
 <?php
     require 'connect.php';
     $artiID = $mysqli->query("SELECT COUNT(*) from artiInfo;");
-    $ID = $_POST[artiID];
-    $contents = $_POST[content];
+    $ID = $_POST["artiID"];
+    $contents = $_POST["content"];
     $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES($ID,$contents);";
     $result = $mysqli->query($insert_sql);
 ?>

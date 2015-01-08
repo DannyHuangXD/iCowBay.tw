9948611dc9390ee584c74a72c4a86b4828b5f3b0
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 11:06:31 +0800

    FUCKING COMMA

--- scripts/new_post.php
+++ scripts/new_post.php
@@ -1,7 +1,7 @@
 <?php
     require 'connect.php';
     $artiID = ＄mysqli->query("SELECT COUNT(*) from artiInfo;");
-    $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES('$_POST[artiID]','$_POST[content]')";
+    $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES('$_POST[artiID]','$_POST[content]');";
     $mysqli->query($insert_sql);
     echo "SUCCESS! your post id is $artiID.";
 ?>

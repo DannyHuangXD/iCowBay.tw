a1c5ffd7cfd58cf6eb75f32c9ab3b0ba3001f72c
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 17:13:43 +0800

    Revert 24adefe..38ae5dd
    This rolls back to commit 24adefeb3aeab6f4d45b683d23cfce04c481fc4a.

--- search.php
+++ search.php
@@ -42,11 +42,7 @@
             $row = mysqli_fetch_row($result);
             $total_records = $row[0];
             $total_pages = ceil($total_records / 20);
-<<<<<<< HEAD:search.php
             echo "<ul class='pagination'>";
-=======
-            echo "<ul>";
->>>>>>> parent of 64dca39... dudududu:scripts/search.php
             for ($i=1; $i<=$total_pages; $i++) {
                 echo "<li><a href='search.php?page=".$i."'>".$i."</a></li>";
             };

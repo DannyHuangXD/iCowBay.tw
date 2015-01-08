36e32a45f4675b4b088aa457fa64f0a6de3a3b36
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 10:36:41 +0800

    .....

--- scripts/search.php
+++ scripts/search.php
@@ -9,7 +9,7 @@
                 $sql_kw = $_REQUEST['sqlkw']; //keyword search variable
                 $sql_search = "SELECT * FROM artiInfo WHERE artitext LIKE '%$sql_kw%'";
                 $result = $mysqli->query($sql_search);
-                $pageCount = celi($rows/3);
+
                 mysqli_free_result($result);
                 $mysqli->close($mysqli);
             ?>

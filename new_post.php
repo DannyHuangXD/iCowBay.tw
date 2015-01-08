ab6c3d84aeebce23442fc29493aeae647a41890a
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-09 00:12:47 +0800

    making posting friendly

--- new_post.php
+++ new_post.php
@@ -20,7 +20,7 @@
         require 'scripts/connect.php';
             $result = $mysqli->query("SELECT * from artiInfo;");
         $id = 1 + mysqli_num_rows($result);
-        $contents = $_POST['content'];
+        $contents = str_replace(chr(13).chr(10),"<br>", $_POST['content']);
         $insert_sql = "INSERT INTO artiInfo" ."(artiID, artitext)"."VALUES('$id', '$contents');";
         $result = $mysqli->query($insert_sql);
         echo "<div class='alert alert-success' role='alert'> It's done. Your article ID is ".$id.".</div>";

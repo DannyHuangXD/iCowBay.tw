a742624cc818b3767845a344fd7c46f22b65b0bb
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-08 11:08:27 +0800

    ;

--- scripts/new_post.php
+++ scripts/new_post.php
@@ -3,5 +3,4 @@
     $artiID = ＄mysqli->query("SELECT COUNT(*) from artiInfo;");
     $insert_sql = "INSERT INTO artiInfo (artiID, artitext) VALUES('$_POST[artiID]','$_POST[content]');";
     $mysqli->query($insert_sql);
-    echo "SUCCESS! your post id is $artiID.";
 ?>

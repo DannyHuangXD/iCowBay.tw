7109b4d99e794cde667a6a71c17698e2189b6f08
DannyHuangXD <dannyhuangxd@gmail.com>
2015-01-09 00:40:36 +0800

    user name

--- new_post.php
+++ new_post.php
@@ -21,7 +21,8 @@
             $result = $mysqli->query("SELECT * from artiInfo;");
         $id = 1 + mysqli_num_rows($result);
         $contents = str_replace(chr(13).chr(10),"<br>", $_POST['content']);
-        $insert_sql = "INSERT INTO artiInfo" ."(artiID, artitext)"."VALUES('$id', '$contents');";
+        $usr_id = $_POST['user_id'];
+        $insert_sql = "INSERT INTO artiInfo" ."(id, artiID, artitext)"."VALUES('$usr_id' ,'$id', '$contents');";
         $result = $mysqli->query($insert_sql);
         echo "<div class='alert alert-success' role='alert'> It's done. Your article ID is ".$id.".</div>";
     ?>
